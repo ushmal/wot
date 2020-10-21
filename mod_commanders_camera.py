513e641893a25a66fe47054129c3fba6be6c0612
marvin <ushmal@gmail.com>
2020-10-21 19:56:11 +0300

    fixes for 1.10.1

--- mod_commanders_camera.py
+++ mod_commanders_camera.py
@@ -1,17 +1,17 @@
 from AvatarInputHandler.DynamicCameras.ArcadeCamera import ArcadeCamera, MinMax
 from AvatarInputHandler.DynamicCameras.StrategicCamera import StrategicCamera
 
-def hook_ArcadeCamera_readCfg(self, dataSec):
-    prev_ArcadeCamera_readCfg(self, dataSec)
-    distRange = MinMax(2, 125)
+def hook_ArcadeCamera_readConfigs(self, dataSec):
+    prev_ArcadeCamera_readConfigs(self, dataSec)
+    distRange = MinMax(2, 150)
     self._userCfg['distRange'] = self._cfg['distRange'] = distRange
 
-prev_ArcadeCamera_readCfg = ArcadeCamera._ArcadeCamera__readCfg
-ArcadeCamera._ArcadeCamera__readCfg = hook_ArcadeCamera_readCfg
+prev_ArcadeCamera_readConfigs = ArcadeCamera._readConfigs
+ArcadeCamera._readConfigs = hook_ArcadeCamera_readConfigs
 
-def hook_StrategicCamera_readCfg(self, dataSec):
-    prev_StrategicCamera_readCfg(self, dataSec)
-    self._userCfg['distRange'] = self._cfg['distRange'] = (40, 150)
+def hook_StrategicCamera_readConfigs(self, dataSec):
+    prev_StrategicCamera_readConfigs(self, dataSec)
+    self._userCfg['distRange'] = self._cfg['distRange'] = (40, 200)
 
-prev_StrategicCamera_readCfg = StrategicCamera._StrategicCamera__readCfg
-StrategicCamera._StrategicCamera__readCfg = hook_StrategicCamera_readCfg
+prev_StrategicCamera_readConfigs = StrategicCamera._readConfigs
+StrategicCamera._readConfigs = hook_StrategicCamera_readConfigs
